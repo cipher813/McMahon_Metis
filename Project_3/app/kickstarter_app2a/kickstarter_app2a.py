5dffdc556da9cad26b199d299250c52eaf121e51
Brian McMahon <30627595+cipher813@users.noreply.github.com>
2018-02-20 21:30:57 -0800

    fixed pull issue

--- Project_3/app/kickstarter_app2a/kickstarter_app2a.py
+++ Project_3/app/kickstarter_app2a/kickstarter_app2a.py
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import pandas as pd
-from flask import Flask, request, render_template
-
-# df = pd.read_csv("kickstarter_bostockscatterplot.csv")
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-=======
 from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path='/static')
@@ -24,4 +9,3 @@ def index():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
->>>>>>> cb01e1c4dd08e8661e121dfce6d8fa7da80b2846
